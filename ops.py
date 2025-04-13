123db7a89a1b0ebef194909ae279e06aa7f1b549
Larianter <lari.vainio@edu.turkuamk.fi>
2025-04-13 22:11:07 +0300

    Finished operations module

--- ops.py
+++ ops.py
@@ -2,81 +2,120 @@
 import math
 
 class Operation:
-    def __init__(self,id):
-        self.id = id
+    idCounter = 1
+
+    #Unique ID generation for each operation
+    def __init__(self):
+        self.id = Operation.idCounter
+        Operation.idCounter += 1
 
 class Addition(Operation):
-    def __init__(self,a,b,id):
-        super().__init__(id)
+    def __init__(self,a,b):
+        super().__init__()
         self.a = a
         self.b = b
 
     def add(self):
-        result = self.a + self.b
-        return  result
+        self.result = self.a + self.b
+        return  self.result
     
     def __str__(self):
-        pass
+        return f"{self.a} + {self.b} = {self.result}"
 
 class Subtraction(Operation):
-    def __init__(self,a,b,id):
-        super().__init__(id)
+    def __init__(self,a,b):
+        super().__init__()
         self.a = a
         self.b = b
 
     def sub(self):
-        result = self.a - self.b
-        return result
+        self.result = self.a - self.b
+        return self.result
     
     def __str__(self):
-        pass
+        return f"{self.a} - {self.b} = {self.result}"
 
 class Division(Operation):
-    def __init__(self,a,b,id):
-        super().__init__(id)
+    def __init__(self,a,b):
+        super().__init__()
         self.a = a
         self.b = b
     
     def div(self):
-        result = self.a / self.b
-        return result
+        self.result = self.a / self.b
+        return self.result
     
     def __str__(self):
-        pass
+        return f"{self.a} / {self.b} = {self.result}"
 
 class Exponent(Operation):
-    def __init__(self,a,b,id):
-        super().__init__(id)
+    def __init__(self,a,b):
+        super().__init__()
         self.a = a
         self.b = b
 
     def exp(self):
-        result = math.pow(self.a,self.b)
-        return result
+        self.result = math.pow(self.a,self.b)
+        return self.result
 
     def __str__(self):
-        pass
+        return f"{self.a}^{self.b} = {self.result}"
 
 class Square(Operation):
-    def __init__(self,a,id):
-        super().__init__(id)
+    def __init__(self,a):
+        super().__init__()
         self.a = a
     
     def sqrt(self):
-        result = math.sqrt(self.a)
-        return result
+        self.result = math.sqrt(self.a)
+        return self.result
 
     def __str__(self):
-        pass
+        return f"sqrt({self.a} = {self.result})"
 
 class Inverse(Operation):
-    def __init__(self,a,id):
-        super().__init__(id)
+    def __init__(self,a):
+        super().__init__()
         self.a = a
 
     def inv(self):
-        result = 1/self.a
-        return result
+        self.result = 1/self.a
+        return self.result
 
     def __str__(self):
-        pass
+        return f"{self.a}^-1 = {self.result}"
+
+'''
+def test():
+    addOperation = Addition(2,2)
+    addOperation.add()
+    print(addOperation)
+    print(addOperation.id)
+
+    subOperation = Subtraction(4,2)
+    subOperation.sub()
+    print(subOperation)
+    print(subOperation.id)
+
+    divOperation = Division(8,4)
+    divOperation.div()
+    print(divOperation)
+    print(divOperation.id)
+
+    expOperation = Exponent(3,3)
+    expOperation.exp()
+    print(expOperation)
+    print(expOperation.id)
+
+    sqrtOperation = Square(64)
+    sqrtOperation.sqrt()
+    print(sqrtOperation)
+    print(sqrtOperation.id)
+
+    invOperation = Inverse(4)
+    invOperation.inv()
+    print(invOperation)
+    print(invOperation.id)
+
+test()
+'''
