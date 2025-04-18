d510267bcadbf2f4550badb12476655bd22da9c0
Nokkoskeitto <pellinennoora@gmail.com>
2025-04-18 22:54:21 +0300

    deleted functions from UI -> to be redone in own module

--- calc_UI.py
+++ calc_UI.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from ops import Addition, Subtraction, Division, Exponent, Square, Inverse, Multiplication #add Ans when it's added to the ops file
+
 class Calculator():
     def __init__(self, root):
         self.bg_color = "#ffc0cb" #As I get to do the UI, I'll decide the colours >:D
@@ -47,67 +47,6 @@ class Calculator():
             button_frame.rowconfigure(i, weight=1)
         for j in range(5):
             button_frame.columnconfigure(j, weight=1)
-
-    def on_click(self, key):
-        if key == "=":
-            try:
-                for op in ['+', '-', '*', '÷', '^', '√', 'Ans']: #Need to add and fix perentesis
-                    if op in self.expression:
-                        a, b = self.expression.split(op)
-                        a = float(a.strip())
-                        b = float(b.strip())
-                        #calling all the operations from the ops file
-                        if op == '+':
-                            result = Addition(a, b).add()
-                        elif op == '-':
-                            result = Subtraction(a, b).sub()
-                        elif op == '*':
-                            result = Multiplication(a, b).mul()
-                        elif op == '÷':
-                            result = Division(a, b).div()
-                        elif op == '^':
-                            result = Exponent(a, b).exp()
-                        elif op == '√':
-                            result = Square(a).sqrt()
-                        #elif op == 'Ans':
-                            #gotta add this to the ops file
-                        break
-                else:
-                    result = self.expression 
-
-                self.last_answer = str(result)
-                self.expression = self.last_answer
-            except Exception as e:
-                result = "Error"
-                self.expression = ""
-
-            self.display.delete(0, tk.END)
-            self.display.insert(tk.END, result)
-        
-        elif key == "x⁻¹":
-            try:
-                current_value = eval(self.expression.replace('÷', '/').replace('^', '**'))
-                result = Inverse(current_value).inv()
-                self.expression = str(result)
-                self.display.delete(0, tk.END)
-                self.display.insert(tk.END, result)
-            except:
-                self.display.delete(0, tk.END)
-                self.display.insert(tk.END, "Error")
-                self.expression = ""
-
-        elif key == "CE":
-            self.expression = ""
-            self.display.delete(0, tk.END)
-
-        elif key == "Del":
-            self.expression = self.expression[:-1]
-            self.display.delete(0, tk.END)
-            self.display.insert(tk.END, self.expression)
-
-        else:
-            self.expression += key
-            self.display.insert(tk.END, key)
 
 # Run the app
 if __name__ == "__main__":
